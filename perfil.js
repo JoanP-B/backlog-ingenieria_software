a99a91a46712c674a7faa3fb9bcde518a8caad23
gljaramilloc <gljaramilc@eafit.edu.co>
2026-04-09 09:49:17 -0500

    guardando cambios antes de integrar ramas

---
+++ perfil.js
@@ -0,0 +1,30 @@
+document.getElementById('profileForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const fields = this.querySelectorAll('[required]');
+    let isValid = true;
+    let firstErrorField = null;
+
+    fields.forEach(field => {
+        field.classList.remove('input-error');
+        
+        // Validar si está vacío o si es un archivo no seleccionado
+        const isEmpty = !field.value.trim();
+        const isFileEmpty = field.type === 'file' && field.files.length === 0;
+
+        if (isEmpty || isFileEmpty) {
+            isValid = false;
+            field.classList.add('input-error');
+            if (!firstErrorField) firstErrorField = field;
+        }
+    });
+
+    if (!isValid) {
+        alert("Favor ingresar información válida en los campos obligatorios.");
+        // CA-03: Foco y cursor en el primer campo vacío
+        firstErrorField.focus();
+    } else {
+        alert("Perfil profesional guardado con éxito. Iniciando scoring de vacantes...");
+        // Aquí se enviaría a la base de datos PostgreSQL mediante una API
+    }
+});
